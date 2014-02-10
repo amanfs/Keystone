779999d2dbeb5c975535dae728e13a31290edd54
amanfs <faman@me.com>
2014-02-10 10:33:37 -0800

    html stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,8 +4,8 @@ angular.module('keystoneApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', { templateUrl: 'views/main.html', controller: 'MainCtrl' })
-      .when('/products', { templateUrl: 'views/product.html', controller: 'ProductCtrl' })
-      .when('/products/:productId', { templateUrl: 'views/products.html', controller: 'ProductCtrl' })
+      .when('/products', { templateUrl: 'views/products.html', controller: 'ProductCtrl' })
+      .when('/products/:productId', { templateUrl: 'views/product.html', controller: 'ProductCtrl' })
       .when('/contact-us', { templateUrl: 'views/contact.html', controller: 'ContactCtrl' })
       .when('/about-us', { templateUrl: 'views/about.html', controller: 'AboutCtrl' })   
       .otherwise({ redirectTo: '/' });
